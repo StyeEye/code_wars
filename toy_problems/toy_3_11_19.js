64576ee9efea04073b242050f2c619d5442f3f6b
StyeEye <ColonialBuff@protonmail.com>
2019-03-11 18:29:10 -0600

    Minor changes to 3/11/19 toy

--- toy_problems/toy_3_11_19.js
+++ toy_problems/toy_3_11_19.js
@@ -1,23 +1,25 @@
 // Problem - Recreate array.filter(), more or less
 
 // Dataset
-const data = [1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90];
+const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90];
 // Expected output - [10,20,30,40,50,60,70,80,90]
 
-// Expects array, and a function that will return true/false given any element as a value
+// Expects array and a function with 1 parameter that returns true/false
+// Returns an array made of elements that made the callback return 'true'
 function filter(array, filterFunc) {
     const output = [];
 
     array.forEach(element => {
-        if(filterFunc(element))
+        if (filterFunc(element))
             output.push(element);
     });
 
     return output;
 }
 
+// Expects a number, returns a Boolean
 function gte10(x) {
-    return x >= 10 ? true : false;
+    return x >= 10;
 }
 
 console.log(data);
